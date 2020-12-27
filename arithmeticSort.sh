c7cf5182f19eaf74fe010038bb325eb294d4259c
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 13:37:31 +0530

    REFACTOR. created a dictionary to store the results of the computations

--- arithmeticSort.sh
+++ arithmeticSort.sh
@@ -1,11 +1,12 @@
 #! /bin/bash -x
 
 #Arithmetic Computation and Sort Assignment
+declare -A operations
 read -p "enter 1st input " a
 read -p "enter 2nd input " b
 read -p "enter 3rd input " c
 
-op1=$(($a+$b*$c))
-op2=$(($a*$b+$c))
-op3=`awk 'BEGIN {print('$c'+'$a'/'$b')}'`
-op4=`awk 'BEGIN {print('$a'%'$b'+'$c')}'`
+operations["op1"]=$(($a+$b*$c))
+operations["op2"]=$(($a*$b+$c))
+operations["op3"]=`awk 'BEGIN {print('$c'+'$a'/'$b')}'`
+operations["op4"]=`awk 'BEGIN {print('$a'%'$b'+'$c')}'`
