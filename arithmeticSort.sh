dda645f7f476642a7f2586073f3675bd2d964125
joel3216 <joeljaxon3216@gmail.com>
2020-12-27 13:27:05 +0530

    ADD. added line to compute c+a/b

--- arithmeticSort.sh
+++ arithmeticSort.sh
@@ -5,5 +5,6 @@ read -p "enter 1st input " a
 read -p "enter 2nd input " b
 read -p "enter 3rd input " c
 
-op1=$((a+b*c))
-op2=$((a*b+c))
+op1=$(($a+$b*$c))
+op2=$(($a*$b+$c))
+op3=`awk 'BEGIN {print('$c'+'$a'/'$b')}'`
